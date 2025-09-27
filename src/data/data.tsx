95c7d3f6066fa3facf79ecd58d4e2f511650e875
waleed31 <88284838+waleed31@users.noreply.github.com>
2025-09-28 01:21:13 +0300

    Update data.tsx

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -114,7 +114,7 @@ export const homePageFeatures = [
 
 
 export const partners = ["partner-9.png",  "partner-4.png","partner-5.png" ,"/partner-2.png",
-   "/partner-3.png","/partner-1.png","partner-6.png","partner-7.png","partner-10.png","partner-11.png","partner-12.png"];
+   "/partner-3.png","/partner-1.png","partner-6.png","partner-7.png","partner-10.png","partner-11.png","partner-12.png","partner-13.png"];
 
 export const services = [
   {
